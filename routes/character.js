d0f8b997214ec61337ef34e6b9f33df014fc56ae
nicoriera <80978348+nicoriera@users.noreply.github.com>
2021-10-15 17:03:19 +0200

    update route character

--- routes/character.js
+++ routes/character.js
@@ -4,23 +4,24 @@ const qs = require("qs");
 const axios = require("axios");
 
 // Route get characters
-router.get("/comics/:characterId", async (req, res) => {
-  console.log("route: /comics/:characterId");
+router.get("/characters", async (req, res) => {
+  console.log("route: /characters");
   console.log(req.query);
   const { limit, skip, name } = req.query;
   let queryParams = qs.stringify({
+    limit: limit,
+    skip: skip,
+    name: name,
     apiKey: process.env.API_KEY_MARVEL,
   });
   try {
-    const characterId = req.params.characterId;
     let response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?${queryParams}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?${queryParams}`
     );
 
     res.status(200).json(response.data);
   } catch (error) {
-    console.log("characterId/comics", error.message);
-    res.status(400).json({ message: error.message });
+    console.error(error.message);
   }
 });
 
