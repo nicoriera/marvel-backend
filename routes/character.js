d573dc5fb3a1e7a65627404281edec0795d7c888
nicoriera <80978348+nicoriera@users.noreply.github.com>
2021-10-15 17:07:31 +0200

    add rout comics characterid

--- routes/character.js
+++ routes/character.js
@@ -25,4 +25,18 @@ router.get("/characters", async (req, res) => {
   }
 });
 
+router.get("/comics/:characterId", async (req, res) => {
+  try {
+    const characterId = req.params.characterId;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log("characterId/comics", error.message);
+    res.status(400).json({ message: error.message });
+  }
+});
+
 module.exports = router;
